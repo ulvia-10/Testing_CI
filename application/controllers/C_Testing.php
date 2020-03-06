03d94a013a62c7e4f8f312f3e3b375eb26196fa3
cahyaekapermana <cahyaekapermana@gmail.com>
2020-03-06 22:55:48 +0700

    Nambah Controller & View

---
+++ application/controllers/C_Testing.php
@@ -0,0 +1,15 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class C_Tesing extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('V_Testing');
+    }
+}
+    
+    /* End of file C_Tesing.php */
